4e241b1bd1ffceb4b808962c5d59141b0ce75aa9
Wilberth Loría <stwilberth@gmail.com>
2023-05-04 01:18:02 -0600

    pido orden por input

--- main.py
+++ main.py
@@ -2,70 +2,98 @@ import time
 import csv
 import pyautogui
 import pyscreeze
+import sys
 
-# Esperar 5 segundos para que tengas tiempo de abrir la ventana que contiene los botones
-print("abra anduin antes de 5 segundos e ignore esta ventana")
-time.sleep(5)
+sleep_time = 3
 
-# Abrir el archivo CSV que tiene la orden
+# definir la función buscar_boton
+def buscar_boton(ruta_imagen):
+    try:
+        boton_x, boton_y = pyautogui.locateCenterOnScreen(ruta_imagen)
+        pyautogui.click(boton_x, boton_y)
+        time.sleep(sleep_time)
+    except TypeError:
+        print(f"Error: {ruta_imagen}")
+        input()
+        sys.exit(1)
+
+# pedir numero de la orde
 
 try:
-    # Código que puede lanzar una excepción
-    with open('orden.csv', 'r') as archivo:
-        lector_csv = csv.reader(archivo)
-        # Recorrer la columna del archivo CSV y escribir el primer dato encontrado
-        for fila in lector_csv:
-            orden = fila[0]
-            print(orden)
-            time.sleep(5)
+    while True:
+    # Pedir al usuario el número de orden
+        orden = input("Ingrese el número de orden: ")
+
+        # Verificar si el usuario ingresó un número de orden
+        if not orden:
+            print("Debe ingresar un número de orden.")
+            time.sleep(1)
+            continue
+
+        # Preguntar al usuario si el número de orden es correcto
+        respuesta = input(f"Ingresó el número de orden {orden}. ¿Es correcto? (s/n): ")
+
+        # Verificar la respuesta del usuario
+        if respuesta.lower() == 's':
+            # Si la respuesta es sí, salir del ciclo while
+            print("Dirijase al sistema anduin, en 5 segundos inicia la automatizacion")
+            time.sleep(6)
             break
+        elif respuesta.lower() == 'n':
+            # Si la respuesta es no, continuar con el ciclo while
+            continue
+        else:
+            # Si la respuesta no es válida, pedir que ingrese una respuesta válida
+            print("Debe ingresar 's' o 'n', ahora repita el proceso")
+            continue
 except Exception as e:
     # Capturamos la excepción y la mostramos
     print("Error: ", e)
-    input()
+    input("Presione enter para salir")
+    sys.exit(1)
 
 # Abrir el archivo CSV
 with open('datos.csv', 'r') as archivo:
     lector_csv = csv.reader(archivo)
 
     # Recorrer la columna del archivo CSV y escribir el primer dato encontrado
-    for fila in lector_csv:
-        codigo_cable = fila[0]
+    try: 
+        for fila in lector_csv:
+            codigo_cable = fila[0]
 
-        #login
-        boton1_x, boton1_y = pyautogui.locateCenterOnScreen('botones/boton1.JPG')
-        pyautogui.click(boton1_x, boton1_y)
-        time.sleep(2)
-        
-        #escribir el codigo del cable
-        pyautogui.typewrite(codigo_cable)
-        pyautogui.press('enter')
-        time.sleep(3)
+            #login
+            buscar_boton('botones/boton1.JPG')
+            
+            #escribir el codigo del cable
+            pyautogui.typewrite(codigo_cable)
+            pyautogui.press('enter')
+            time.sleep(sleep_time)
 
-        #da clic al boton de linkear
-        boton2_x, boton2_y = pyautogui.locateCenterOnScreen('botones/boton2.JPG')
-        pyautogui.click(boton2_x, boton2_y)
-        time.sleep(2)
+            #da clic al boton de linkear
+            buscar_boton('botones/boton2.JPG')
 
-        #da clic al boton cuando pregunsta si desea linkear
-        pyautogui.press('enter')
-        time.sleep(4)
+            #da clic al boton cuando pregunsta si desea linkear
+            pyautogui.press('enter')
+            time.sleep(4)
 
-        #introduce el numero de orden
-        pyautogui.typewrite(orden)
-        pyautogui.press('enter')
-        time.sleep(3)
+            #introduce el numero de orden
+            pyautogui.typewrite(orden)
+            pyautogui.press('enter')
+            time.sleep(3)
 
-        #enter a la ventana emergente de que se agrego correctamente
-        pyautogui.press('enter')
+            #enter a la ventana emergente de que se agrego correctamente
+            pyautogui.press('enter')
 
-        #da clic al boton logof
-        boton3_x, boton3_y = pyautogui.locateCenterOnScreen('botones/logof.JPG')
-        pyautogui.click(boton3_x, boton3_y)
-        time.sleep(2)
+            #da clic al boton logof
+            buscar_boton('botones/logof.JPG')
 
-        #repite el ciclo
-        print('cable ', codigo_cable, ' agregado')
+            #repite el ciclo
+            print('cable ', codigo_cable, ' agregado')
+    
+    except Exception as e:
+        # Capturamos la excepción y la mostramos
+        print("Error: ", e)
+        input()
 
 print("El script ha finalizado. Presione Enter para salir.")
 input()
