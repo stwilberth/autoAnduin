1a0fe91b40b1f5b9b00d3afbe28739fbe97335e7
Wilberth Loría <stwilberth@gmail.com>
2023-05-07 11:08:59 -0600

    cambio de busqueda de botones por por posicion del mouse

--- main.py
+++ main.py
@@ -3,97 +3,150 @@ import csv
 import pyautogui
 import pyscreeze
 import sys
+import os
 
-sleep_time = 3
+sleep_time = 1
 
-# definir la función buscar_boton
-def buscar_boton(ruta_imagen):
+# verificar coordenadas
+# si están procedo automatizacion, tienen que ser mayor que cero e igual a 3 creo
+# sino inicio configuracion
+# pido coordenadas
+
+def pedirCoordenadas():
     try:
-        boton_x, boton_y = pyautogui.locateCenterOnScreen(ruta_imagen)
-        pyautogui.click(boton_x, boton_y)
-        time.sleep(sleep_time)
-    except TypeError:
-        print(f"Error: {ruta_imagen}")
-        input()
-        sys.exit(1)
+        coordenadas = tuple()
 
-# pedir numero de la orde
+        for i in range(3):
+            contador_regresivo(5)
+            posicion_clic = pyautogui.position()
+            coordenadas += (posicion_clic,)
+            print(f"Se agregó {len(coordenadas)} coordenadas: {posicion_clic}")
+            input("Enter para continuar")
 
-try:
-    while True:
-    # Pedir al usuario el número de orden
-        orden = input("Ingrese el número de orden: ")
-
-        # Verificar si el usuario ingresó un número de orden
-        if not orden:
-            print("Debe ingresar un número de orden.")
-            time.sleep(1)
-            continue
-
-        # Preguntar al usuario si el número de orden es correcto
-        respuesta = input(f"Ingresó el número de orden {orden}. ¿Es correcto? (s/n): ")
-
-        # Verificar la respuesta del usuario
-        if respuesta.lower() == 's':
-            # Si la respuesta es sí, salir del ciclo while
-            print("Dirijase al sistema anduin, en 5 segundos inicia la automatizacion")
-            time.sleep(6)
-            break
-        elif respuesta.lower() == 'n':
-            # Si la respuesta es no, continuar con el ciclo while
-            continue
-        else:
-            # Si la respuesta no es válida, pedir que ingrese una respuesta válida
-            print("Debe ingresar 's' o 'n', ahora repita el proceso")
-            continue
-except Exception as e:
-    # Capturamos la excepción y la mostramos
-    print("Error: ", e)
-    input("Presione enter para salir")
-    sys.exit(1)
-
-# Abrir el archivo CSV
-with open('datos.csv', 'r') as archivo:
-    lector_csv = csv.reader(archivo)
-
-    # Recorrer la columna del archivo CSV y escribir el primer dato encontrado
-    try: 
-        for fila in lector_csv:
-            codigo_cable = fila[0]
-
-            #login
-            buscar_boton('botones/boton1.JPG')
-            
-            #escribir el codigo del cable
-            pyautogui.typewrite(codigo_cable)
-            pyautogui.press('enter')
-            time.sleep(sleep_time)
-
-            #da clic al boton de linkear
-            buscar_boton('botones/boton2.JPG')
-
-            #da clic al boton cuando pregunsta si desea linkear
-            pyautogui.press('enter')
-            time.sleep(4)
-
-            #introduce el numero de orden
-            pyautogui.typewrite(orden)
-            pyautogui.press('enter')
-            time.sleep(3)
-
-            #enter a la ventana emergente de que se agrego correctamente
-            pyautogui.press('enter')
-
-            #da clic al boton logof
-            buscar_boton('botones/logof.JPG')
-
-            #repite el ciclo
-            print('cable ', codigo_cable, ' agregado')
+        escribir_coordenadas_csv('coordenadas.csv', coordenadas)
+        input("solicitud de coordenadas finalizada, presione enter para continuar")
+        return coordenadas
     
     except Exception as e:
         # Capturamos la excepción y la mostramos
         print("Error: ", e)
-        input()
+        input("Presione enter para salir")
+        sys.exit(1)
 
-print("El script ha finalizado. Presione Enter para salir.")
-input()
+def contador_regresivo(tiempo):
+    input(f"Presione enter para obtener la posicion del mouse, tiene {str(tiempo)} segundos para posicionar el mouse")
+    inicio = time.time()
+    tiempo_transcurrido = 0
+    while tiempo_transcurrido < tiempo:
+        tiempo_restante = tiempo - tiempo_transcurrido
+        print(f"Tiempo restante: {tiempo_restante} segundos", end="\r")
+        time.sleep(1)
+        tiempo_transcurrido = int(time.time() - inicio)
+    print("Tiempo terminado!")
+
+def leer_coordenadas_csv(ruta_archivo):
+    coordenadas = ()
+    if os.stat(ruta_archivo).st_size == 0:
+        return coordenadas
+    else:
+        with open(ruta_archivo, mode='r') as archivo_csv:
+            reader = csv.reader(archivo_csv)
+            next(reader)  # saltar la primera fila con los encabezados
+            for fila in reader:
+                x, y = map(int, fila)
+                coordenadas += ((x, y),)
+        return coordenadas
+
+def escribir_coordenadas_csv(ruta_archivo, coordenadas):
+    with open(ruta_archivo, mode='w', newline='') as archivo_csv:
+        writer = csv.writer(archivo_csv)
+        writer.writerow(['x', 'y'])
+        for x, y in coordenadas:
+            writer.writerow([x, y])
+
+def pedirOrden():
+    try:
+        while True:
+            orden = input("Ingrese el número de orden: ")
+            
+            if not orden:
+                continue
+            
+            respuesta = input("¿Es correcto? (s/n):")
+            if respuesta.lower() == 's':
+                break
+            else:
+                continue
+
+        return orden
+    except Exception as e:
+        # Capturamos la excepción y la mostramos
+        print("Error: ", e)
+        input("Presione enter para salir")
+        sys.exit(1)
+
+# Abrir el archivo CSV
+def data_csv(coordenadas, orden):
+    with open('datos.csv', 'r') as archivo:
+        lector_csv = csv.reader(archivo)
+
+        # Recorrer la columna del archivo CSV y escribir el primer dato encontrado
+        try: 
+            for fila in lector_csv:
+                codigo_cable = fila[0]
+
+                #login
+                x, y = coordenadas[0]
+                pyautogui.click(x, y)
+                time.sleep(sleep_time)
+                
+                #escribir el codigo del cable
+                print(codigo_cable)
+                pyautogui.typewrite(codigo_cable)
+                pyautogui.press('enter')
+                time.sleep(sleep_time)
+
+                #da clic al boton de linkear
+                x, y = coordenadas[1]
+                pyautogui.click(x, y)
+                time.sleep(sleep_time)
+
+                #da clic al boton cuando pregunsta si desea linkear
+                pyautogui.press('enter')
+                time.sleep(sleep_time)
+
+                #introduce el numero de orden
+                pyautogui.typewrite(orden)
+                pyautogui.press('enter')
+                time.sleep(sleep_time)
+
+                #enter a la ventana emergente de que se agrego correctamente
+                pyautogui.press('enter')
+
+                #da clic al boton logof
+                x, y = coordenadas[2]
+                pyautogui.click(x, y)
+
+                #repite el ciclo
+                print('cable ', codigo_cable, ' agregado')
+                time.sleep(sleep_time)
+        
+        except Exception as e:
+            # Capturamos la excepción y la mostramos
+            print("Error: ", e)
+            input("Presione enter para salir")
+
+def start():
+    coordenadas = leer_coordenadas_csv('coordenadas.csv')
+    
+    if len(coordenadas) < 3:
+        coordenadas = pedirCoordenadas()
+
+    orden = pedirOrden()
+
+    data_csv(coordenadas, orden)
+
+# iniciar el programa
+start()
+    
+input("La automatización ha finalizado. Presione Enter para salir")
