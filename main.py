4f3f9205df5d24f70fbd842e75fc7b4733a510f4
Wilberth Loría <stwilberth@gmail.com>
2023-05-10 00:57:13 -0600

    se corrigio el orden de los procesos

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import pyautogui
 import sys
 import os
 
-sleep_time = int(input("Digete en segundos el tiempo de espera en cada iteracion: "))
+sleep_time = int(input("Digete el tiempo de espera en segundo entre cada paso: "))
 
-# verificar coordenadas
-# si están procedo automatizacion, tienen que ser mayor que cero e igual a 3 creo
-# sino inicio configuracion
-# pido coordenadas
+def contador_regresivo(tiempo):
+    inicio = time.time()
+    tiempo_transcurrido = 0
+    while tiempo_transcurrido < tiempo:
+        tiempo_restante = tiempo - tiempo_transcurrido
+        print(f"Time: {tiempo_restante} ", end="\r")
+        time.sleep(1)
+        tiempo_transcurrido = int(time.time() - inicio)
 
 def pedirCoordenadas():
     try:
         coordenadas = tuple()
 
         for i in range(3):
-            contador_regresivo(7)
+            tiempo = 8
+            input(f"Presione enter para obtener la posicion del mouse, tiene {str(tiempo)} segundos para posicionar el mouse")
+            contador_regresivo(tiempo)
             posicion_clic = pyautogui.position()
             coordenadas += (posicion_clic,)
             print(f"Se agregó {len(coordenadas)} coordenadas: {posicion_clic}")
@@ -31,17 +37,6 @@ def pedirCoordenadas():
         print("Error: ", e)
         input("Presione enter para salir")
         sys.exit(1)
-
-def contador_regresivo(tiempo):
-    input(f"Presione enter para obtener la posicion del mouse, tiene {str(tiempo)} segundos para posicionar el mouse")
-    inicio = time.time()
-    tiempo_transcurrido = 0
-    while tiempo_transcurrido < tiempo:
-        tiempo_restante = tiempo - tiempo_transcurrido
-        print(f"Tiempo restante: {tiempo_restante} segundos", end="\r")
-        time.sleep(1)
-        tiempo_transcurrido = int(time.time() - inicio)
-    print("Tiempo terminado!")
 
 def leer_coordenadas_csv(ruta_archivo):
     coordenadas = ()
@@ -73,11 +68,10 @@ def pedirOrden():
             
             respuesta = input("¿Es correcto? (s/n):")
             if respuesta.lower() == 's':
-                break
+                return orden
             else:
                 continue
 
-        return orden
     except Exception as e:
         # Capturamos la excepción y la mostramos
         print("Error: ", e)
@@ -97,37 +91,49 @@ def data_csv(coordenadas, orden):
                 #login
                 x, y = coordenadas[0]
                 pyautogui.click(x, y)
-                time.sleep(sleep_time)
+                print('logueando')
+                contador_regresivo(sleep_time)
                 
                 #escribir el codigo del cable
                 pyautogui.write(codigo_cable)
                 pyautogui.press('enter')
-                time.sleep(sleep_time)
+                print('Escribiendo el codigo del cable: ', codigo_cable)
+                contador_regresivo(sleep_time)
 
                 #da clic al boton de linkear
                 x, y = coordenadas[1]
                 pyautogui.click(x, y)
-                time.sleep(sleep_time)
-
-                #da clic al boton cuando pregunsta si desea linkear
-                pyautogui.press('enter')
-                time.sleep(sleep_time)
+                print('Clic boton linkear')
+                contador_regresivo(sleep_time)
 
                 #introduce el numero de orden
                 pyautogui.typewrite(orden)
                 pyautogui.press('enter')
-                time.sleep(sleep_time)
+                print('Escribiendo el numero de orden: ', orden)
+                contador_regresivo(sleep_time)
+
+                #da clic al boton cuando pregunsta si desea linkear
+                pyautogui.press('enter')
+                print('Presionando enter')
+                contador_regresivo(sleep_time)
 
                 #enter a la ventana emergente de que se agrego correctamente
                 pyautogui.press('enter')
+                print('Presionando otra vez enter')
+                contador_regresivo(sleep_time)
 
                 #da clic al boton logof
                 x, y = coordenadas[2]
                 pyautogui.click(x, y)
+                print('Clic boton logof')
+                contador_regresivo(sleep_time)
 
                 #repite el ciclo
-                print('cable ', codigo_cable, ' agregado')
-                time.sleep(sleep_time)
+                print('cable linkeado')
+                print('...')
+                print('...')
+                print('...')
+
         
         except Exception as e:
             # Capturamos la excepción y la mostramos
@@ -135,16 +141,24 @@ def data_csv(coordenadas, orden):
             input("Presione enter para salir")
 
 def start():
+
     coordenadas = leer_coordenadas_csv('coordenadas.csv')
-    
+
     if len(coordenadas) < 3:
         coordenadas = pedirCoordenadas()
 
     orden = pedirOrden()
+    
+    while True:
+        print("Empezando automatizacion, de clic en la ventana de anduin")
+        contador_regresivo(10)
 
-    data_csv(coordenadas, orden)
+        data_csv(coordenadas, orden)
+        
+        input("La automatización ha finalizado. Presione Enter para empezar otra lista de cables.")
+        print('...')
+        input("Asegurese de borrar la lista del archivo datos.csv y escanear los nuevos cables")
 
 # iniciar el programa
 start()
     
-input("La automatización ha finalizado. Presione Enter para salir")
