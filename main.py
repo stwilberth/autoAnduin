a733a1e9e5847690431b46204084f12b4d79a6f7
Wilberth Loría <stwilberth@gmail.com>
2023-05-12 12:28:40 -0600

    escaneo desde el terminal

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 import pyautogui
 import sys
 import os
+from colorama import init, Fore, Style
 
 sleep_time = int(input("Digete el tiempo de espera en segundo entre cada paso: "))
 
@@ -78,57 +79,78 @@ def pedirOrden():
         input("Presione enter para salir")
         sys.exit(1)
 
+def scan_codes():
+    num_codes = int(input("Ingrese la cantidad de códigos a escanear: "))
+    codes = set()
+    count = 0
+    while len(codes) < num_codes:
+        while True:
+            code = input("Ingrese el código a escanear: ")
+            if code.strip() == "":
+                print("No puede estar vacío")
+            elif " " in code:
+                print("No puede contener espacios en blanco")
+            else:
+                break
+        if code not in codes:
+            codes.add(code)
+            count += 1
+            print(f"Escaneado código {count}/{num_codes}")
+        else:
+            print("El código ya está escaneado")
+    sorted_codes = sorted(codes)
+    print("Códigos escaneados:")
+    for code in sorted_codes:
+        print(code)
+    
+    return sorted_codes
+
 # Abrir el archivo CSV
-def data_csv(coordenadas, orden):
-    with open('datos.csv', 'r') as archivo:
-        lector_csv = csv.reader(archivo)
+def automation(coordenadas, orden, sort_cables):
+    try:             
+        for i, codigo_cable in enumerate(sort_cables):
 
-        # Recorrer la columna del archivo CSV y escribir el primer dato encontrado
-        try: 
-            for fila in lector_csv:
-                codigo_cable = fila[0]
+            #login
+            x, y = coordenadas[0]
+            pyautogui.click(x, y)
+            time.sleep(sleep_time)
+            
+            #escribir el codigo del cable
+            pyautogui.write(codigo_cable)
+            pyautogui.press('enter')
+            time.sleep(sleep_time)
 
-                #login
-                x, y = coordenadas[0]
-                pyautogui.click(x, y)
-                contador_regresivo(sleep_time)
-                
-                #escribir el codigo del cable
-                pyautogui.write(codigo_cable)
-                pyautogui.press('enter')
-                contador_regresivo(sleep_time)
+            #da clic al boton de linkear
+            x, y = coordenadas[1]
+            pyautogui.click(x, y)
+            time.sleep(sleep_time)
 
-                #da clic al boton de linkear
-                x, y = coordenadas[1]
-                pyautogui.click(x, y)
-                contador_regresivo(sleep_time)
+            #introduce el numero de orden
+            pyautogui.typewrite(orden)
+            pyautogui.press('enter')
+            time.sleep(sleep_time)
 
-                #introduce el numero de orden
-                pyautogui.typewrite(orden)
-                pyautogui.press('enter')
-                contador_regresivo(sleep_time)
+            #da clic al boton cuando pregunsta si desea linkear
+            pyautogui.press('enter')
+            time.sleep(sleep_time)
 
-                #da clic al boton cuando pregunsta si desea linkear
-                pyautogui.press('enter')
-                contador_regresivo(sleep_time)
+            #enter a la ventana emergente de que se agrego correctamente
+            pyautogui.press('enter')
+            time.sleep(sleep_time)
 
-                #enter a la ventana emergente de que se agrego correctamente
-                pyautogui.press('enter')
-                contador_regresivo(sleep_time)
+            #da clic al boton logof
+            x, y = coordenadas[2]
+            pyautogui.click(x, y)
+            time.sleep(sleep_time)
 
-                #da clic al boton logof
-                x, y = coordenadas[2]
-                pyautogui.click(x, y)
-                contador_regresivo(sleep_time)
-
-                #repite el ciclo
-                print(codigo_cable)
-
-        
-        except Exception as e:
-            # Capturamos la excepción y la mostramos
-            print("Error: ", e)
-            input("Presione enter para salir")
+            #repite el ciclo
+            numero = i + 1
+            print(f'Escaneado {numero}/{len(sort_cables)}: {codigo_cable}')
+    
+    except Exception as e:
+        # Capturamos la excepción y la mostramos
+        print("Error: ", e)
+        input("Presione enter para salir")
 
 def start():
 
@@ -140,14 +162,25 @@ def start():
     orden = pedirOrden()
 
     while True:
-        print("Empezando automatizacion, de clic en la ventana de anduin")
+        cables = scan_codes()
+        init()
+        print(Fore.YELLOW + 'Cierre e inicie sesion en anduin antes de continuar' + Fore.RESET)
+        input('En 10 segundos iniciará la automatizacion, Presione enter para continuar')
+        print(Fore.YELLOW + 'Asegurese de posicionarse en la ventana de Anduin' + Fore.RESET)
         contador_regresivo(10)
 
-        data_csv(coordenadas, orden)
+        automation(coordenadas, orden, cables)
         
-        input("La automatización ha finalizado. Presione Enter para empezar otra lista de cables.")
-        print('...')
-        input("Asegurese de borrar la lista del archivo datos.csv y escanear los nuevos cables")
+        init()  # Inicializar colorama
+
+        print(Fore.GREEN + "##############################")
+        print(Fore.GREEN + "#                            #")
+        print(Fore.GREEN + "#       TAREA FINALIZADA     #")
+        print(Fore.GREEN + "#                            #")
+        print(Fore.GREEN + "##############################")
+        print(Style.RESET_ALL)  # Resetear los estilos de color
+        input("Presione Enter para INGRESAR otra lista de cables")
+
 
 # iniciar el programa
 start()
